4ae5e515a6ea4898bc2b86d17c1df1a8d0089afd
Vladimir <markule@mail.ru>
2019-09-20 16:25:02 +0300

    Reorg file struct

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM php:7.2-cli
-COPY app/ws /usr/src/ws
-WORKDIR /usr/src/ws
-EXPOSE 8889
-CMD [ "php", "./server.php" ]
--- app/head.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-<meta charset='utf-8'>
-<title>The Wall</title>
-<link rel='stylesheet' type='text/css' href='style/style.css' />
-<script type='text/javascript' src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
-<script type='text/javascript' src='js/start.js' ></script>
-</head>
